ae836013162770b2f0967c51265ca0f49135978d
Shem Karafa <karafashem@gmail.com>
2024-08-12 08:48:32 +0300

    create src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from 'react';
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+    <h1>Hi, I'm Natasha</h1>
+    <img src="/home/natasha/Downloads/Picture2.jpg" alt="Natasha" />
+      <h2>About Me</h2>
+      <p>I'm a full stack software developer</p>
+      <div>
+        <a href="https://github.com/NatashaMakungu">GitHub</a>
+        <a href="https://www.linkedin.com/in/natasha-makungu-5279b4257/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a>
+      </div>
+  </div>
+  );
 }
 
 export default App;
