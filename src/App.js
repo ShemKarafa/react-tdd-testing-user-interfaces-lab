4a9b2de33c2240c2c4e023c5a6aa99c845d7c6cb
Shem Karafa <karafashem@gmail.com>
2024-08-12 08:49:26 +0300

    create src

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 function App() {
   return (
   <div>
-    <h1>Hi, I'm Natasha</h1>
-    <img src="/home/natasha/Downloads/Picture2.jpg" alt="Natasha" />
+    <h1>Hi, I'm Shem</h1>
+    <img src="/home/shem/Downloads/Picture2.jpg" alt="Shem" />
       <h2>About Me</h2>
       <p>I'm a full stack software developer</p>
       <div>
-        <a href="https://github.com/NatashaMakungu">GitHub</a>
-        <a href="https://www.linkedin.com/in/natasha-makungu-5279b4257/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a>
+        <a href="https://github.com/ShemKarafa">GitHub</a>
+        <a href="https://www.linkedin.com/in/nicolous-hunter-8253882a5/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a>
       </div>
   </div>
   );
